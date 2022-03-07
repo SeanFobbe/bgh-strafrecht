546bb5824ada4055339b7cca9da6a1836a34446c
seanfobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-03-07 08:25:17 +0100

    add function body

--- functions/mgsub_clean.R
+++ functions/mgsub_clean.R
@@ -1,6 +1,10 @@
 mgsub_clean <- function(text,
                         replacement.table){
 
-    
+    out <- mgsub::mgsub(text,
+                        replacement.table$pattern,
+                        replacement.table$replacement)
 
-    }
+    return(out)
+
+}
