6106cf14cb8ea9b520dea3d3b37c9db331b3f08c
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-07-26 01:54:32 +0200

    add future pdf fun

---
+++ functions/f.future_pdf_ocr.R
@@ -0,0 +1,117 @@
+
+
+
+
+
+
+f.future_pdf_ocr <- function(x,
+                        outputdir = NULL,
+                        quiet = TRUE,
+                           jobs = round(parallel::detectCores() / 4)){
+
+    ## Timestamp: Begin
+    begin.extract <- Sys.time()
+
+    ## Intro messages
+    if(quiet == FALSE){
+        message(paste("Begin at:", begin.extract))
+        message(paste("Processing", length(x), "PDF files."))
+    }
+
+    ## Perform conversion from PDF to TXT
+    invisible(future.apply::future_lapply(x,
+                                          pdf_extract_single,
+                                          outputdir = outputdir,
+                                          future.seed = TRUE))
+
+
+    ## Construct full list of TXT names
+    txt.names <- gsub("\\.pdf$",
+                      "\\.txt",
+                      x,
+                      ignore.case = TRUE)
+
+    ## Check list of TXT files in folder
+    txt.results <- file.exists(txt.names)
+    
+    ## Timestamp: End
+    end.extract <- Sys.time()
+
+    ## Duration
+    duration.extract <- end.extract - begin.extract
+
+    
+    ## Outro messages
+    if(quiet == FALSE){
+        message(paste0("Successfully processed ",
+                       sum(txt.results),
+                       " PDF files. ",
+                       sum(!txt.results),
+                       " PDF files failed."))
+        
+        message(paste0("Runtime was ",
+                       round(duration.extract,
+                             digits = 2),
+                       " ",
+                       attributes(duration.extract)$units,
+                       "."))
+        
+        message(paste0("Ended at: ",
+                       end.extract))
+    }
+
+}
+
+
+
+
+
+
+pdf_ocr_single <- function(x,
+                           dpi = 300,
+                           lang = "eng",
+                           output = "pdf txt",
+                           outputdir = NULL){
+    
+
+    name.tiff <- gsub("\\.pdf",
+                      "\\.tiff",
+                      x)
+    
+    name.out <- gsub("\\.pdf",
+                     "_TESSERACT",
+                     x)
+    
+    system2("convert",
+            paste("-density",
+                  dpi,
+                  "-depth 8 -compress LZW -strip -background white -alpha off",
+                  file,
+                  name.tiff))
+    
+    system2("tesseract",
+            paste(name.tiff,
+                  name.out,
+                  "-l",
+                  lang,
+                  output))
+    
+    unlink(name.tiff)
+
+    
+
+    ## Alternate Folder Option
+    if (!is.null(outputdir)){
+        
+        txtname <- file.path(outputdir, basename(txtname))
+        
+        }
+    
+    ## Write TXT to Disk
+    utils::write.table(pdf.extracted,
+                       txtname,
+                       quote = FALSE,
+                       row.names = FALSE,
+                       col.names = FALSE)
+    
+}
