3b10f225742210330e9d73e6d81c54557a2f4eb6
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-07-26 02:53:56 +0200

    change cores to jobs

--- functions/f.tar_pdf_ocr.R
+++ functions/f.tar_pdf_ocr.R
@@ -5,15 +5,15 @@ f.tar_pdf_ocr <- function(x,
                           dpi = 300,
                           lang = "eng",
                           output = "pdf txt",
-                          dir.out.pdf,
-                          dir.out.txt,
+                          dir.out.pdf = "pdf_tesseract",
+                          dir.out.txt = "txt_tesseract",
                           quiet = TRUE,
                           jobs = parallel::detectCores()){
 
     dir.create("temp_tesseract")
 
     plan(multicore,
-         workers = cores)
+         workers = jobs)
 
 
     f.future_pdf_ocr(x = x,
