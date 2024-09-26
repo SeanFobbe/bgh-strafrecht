96cc098d38c00901fb69a8998e2df66f008a4b46
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2024-09-26 22:28:40 +0200

    fix container naming

--- docker-delete-all-data.sh
+++ docker-delete-all-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-time docker build -t ce-bgh:4.4.0 .
+time docker build -t bgh-strafrecht:4.4.0 .
 
-time docker-compose run --rm ce-bgh Rscript delete_all_data.R
+time docker-compose run --rm bgh-strafrecht Rscript delete_all_data.R
