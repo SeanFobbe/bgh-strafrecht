37b11dc720ad71f1142b41d280fb751013a3976b
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2024-09-30 01:22:33 +0200

    finish date fun

--- functions/f.var_date.R
+++ functions/f.var_date.R
@@ -12,11 +12,65 @@
 
 
 
-f.var_date <- function(x,){
+f.var_date <- function(x,
+                       limit = 2000){
 
-    reduced <- substr(x, 1, 2000)
+    reduced <- substr(x, 1, limit)
 
-    date <- stringi::stri_extract_first(str = reduced,
-                                        regex = "")
+    months.german <- c("Januar", "Februar", "März", "April", "Mai", "Juni",
+                       "Juli", "August", "September", "Oktober", "November", "Dezember")
     
-    }
+    date.string <- stringi::stri_extract_first(str = reduced,
+                                               regex = paste0("(Beschluss|Beschluß|Urteil)",
+                                                              "\\s*vom\\s*",
+                                                              "[0-9]{1,2}\\.\\s*",
+                                                              "(",
+                                                              paste0(months.german,
+                                                                     collapse = "|"),
+                                                              ")\\s*",
+                                                              "[0-9]{4}"))
+
+    date.string <- stringi::stri_replace_all(str = date.string,
+                                             regex = "\\s+",
+                                             replacement = " ")
+
+    date.string <- trimws(date.string)
+
+
+
+    date.string <- stringi::stri_replace_all(str = date.string,
+                                             regex = ".+ vom ",
+                                             replacement = "")
+
+    date.string <- stringi::stri_replace_all(str = date.string,
+                                             regex = paste0(" *", months.german, " *"),
+                                             replacement = paste0(formatC(1:12,
+                                                                          width = 2,
+                                                                          flag = "0"),
+                                                                  "."),
+                                             vectorize_all = FALSE)
+
+
+    date.string <- stringi::stri_replace_all(str = date.string,
+                                             regex = "^([0-9])\\.",
+                                             replacement = "0$1\\.")
+
+    date <- as.Date(date.string, format =  c("%d.%m.%Y"))
+        
+   
+    
+    return(date)
+    
+}
+
+
+
+length(date)
+unique(date)
+
+
+    date.ordered[1:200]
+    
+    date.string[1:400]
+    
+    
