5178f72cd159e6b54a50c23a41667da2674daa4c
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2024-09-30 23:21:06 +0200

    wording

--- functions/f.tar_multihashes.R
+++ functions/f.tar_multihashes.R
@@ -1,4 +1,3 @@
-
 #' Parallel Computation of SHA2 and SHA3 Hashes
 
 #' This function parallelizes computation of both SHA2-256 and SHA3-512 hashes for an arbitrary number of files. The function requires the system "openssl" library.
@@ -55,7 +54,9 @@ f.tar_multihashes <- function(x,
 
 #' This function parallelizes computation of both SHA2-256 and SHA3-512 hashes for an arbitrary number of files. It returns a data frame of file names, SHA2-256 hashes and SHA3-512 hashes. The function requires the existence of the openssl library (RPM) on the system.
 #'
-#' Please note that you must declare your own future evaluation strategy prior to using the function to enable parallelization. By default the function will be evaluated sequentially. On Windows, use future::plan(multisession, workers = n), on Linux/Mac, use future::plan(multicore, workers = n), where n stands for the number of CPU cores you wish to use. Due to the need to read/write to the disk the function may not work properly on high-performance clusters.
+#' Please note that you must declare your own future evaluation strategy prior to using the function to enable parallelization. By default the function will be evaluated sequentially. On Windows, use future::plan(multisession, workers = n), on Linux/Mac, use future::plan(multicore, workers = n), where n stands for the number of CPU cores you wish to use.
+#'
+#' Due to the need to read from the disk this function may not work properly on high-performance clusters.
 
 
 #' @param x Character. A vector of filenames or paths.
