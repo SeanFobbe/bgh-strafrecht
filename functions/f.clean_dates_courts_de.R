a75ec35a9b6852b6660bdafc1a15a156ac3edf74
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2024-09-30 23:01:07 +0200

    add f.year iso fun to clean dates script

--- functions/f.clean_dates_courts_de.R
+++ functions/f.clean_dates_courts_de.R
@@ -28,3 +28,29 @@ f.clean_dates_courts_de <- function(x,
     return(x)
     
 }
+
+
+
+
+
+#' f.year.iso
+#'
+#' Tranform Double-Digit Years to ISO Year Format
+#' 
+#' Transforms double-digit years (YY) to four-digit years (YYYY). Based on the assumption that years above a certain boundary belong to the 20th century and years at or below that boundary belong to the 21st century.
+#'
+#' 
+#' @param year Integer. A vector of two-digit years.
+#' @param boundary Integer. The boundary year. Default is 50 (= 1950). 
+
+
+f.year.iso <- function(year,
+                       boundary = 50){
+    
+    out <- ifelse(year > boundary,
+                  1900 + year,
+                  2000 + year)
+
+    return(out)
+    
+}
