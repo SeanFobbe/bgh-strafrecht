cb804621084a05cae2f5d2d0261b339517775547
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2024-09-30 22:53:06 +0200

    clarify year.iso fun

--- functions/f.year.iso.R
+++ functions/f.year.iso.R
@@ -1,14 +1,21 @@
-#'## f.year.iso: Tranform Double-Digit Years to ISO Year Format
-#' This function transforms double-digit years (YY) to four-digit years as per the ISO standard (YYYY). It is based on the assumption that years above a certain boundary belong to the 20th century and years at or below that boundary belong to the 21st century.
+#' f.year.iso
 #'
+#' Tranform Double-Digit Years to ISO Year Format
+#' 
+#' Transforms double-digit years (YY) to four-digit years (YYYY). Based on the assumption that years above a certain boundary belong to the 20th century and years at or below that boundary belong to the 21st century.
 #'
-#'
+#' 
 #' @param inputyear A vector of two-digit years.
 #' @param boundary The boundary year. Default is 50 (=1950). 
 
 
-f.year.iso <- function(inputyear, boundary=50){
-    ifelse(inputyear > boundary,
-           1900+inputyear,
-           2000+inputyear)
+f.year.iso <- function(year,
+                       boundary = 50){
+    
+    out <- ifelse(year > boundary,
+                  1900 + year,
+                  2000 + year)
+
+    return(out)
+    
 }
