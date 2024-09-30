8ed6e4385ed998263d4d0a24c072d431c81397de
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2024-09-30 22:56:08 +0200

    fix func docs

--- functions/f.year.iso.R
+++ functions/f.year.iso.R
@@ -5,8 +5,8 @@
 #' Transforms double-digit years (YY) to four-digit years (YYYY). Based on the assumption that years above a certain boundary belong to the 20th century and years at or below that boundary belong to the 21st century.
 #'
 #' 
-#' @param inputyear A vector of two-digit years.
-#' @param boundary The boundary year. Default is 50 (=1950). 
+#' @param year Integer. A vector of two-digit years.
+#' @param boundary Integer. The boundary year. Default is 50 (= 1950). 
 
 
 f.year.iso <- function(year,
