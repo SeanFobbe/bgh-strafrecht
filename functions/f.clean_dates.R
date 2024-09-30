090d2e1afaa795e764ab9e2d24a0037a31adde56
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2024-09-30 22:57:32 +0200

    translate and revise clean dates fun

--- functions/f.clean_dates.R
+++ functions/f.clean_dates.R
@@ -1,27 +1,29 @@
-#' Daten säubern und nach Datum sortieren
+#' f.clean_dates_courts_de
 #'
-#' Ein Datensatz mit Datums-Variable und Jahres-Variable wird in ISO-Formate konvertiert und nach Datum sortiert.
+#' Specific to German court datasets produced by Fobbe. Converts decision date variable to IDate, computes decision year from date variable, computes four-digit incoming year from two-digit incoming year and sorts dataset by date.
+#'
 
-#' @param x Data.table. Muss Variable "datum", und "eingangsjahr_az" enthalten.
+#' @param x Data.table. Must contain variable "datum" and "eingangsjahr_az".
 
-#' @param return Data.table. Datensatz mit ISO-standardisierten Jahresangaben und nach Datum sortiert.
+#' @param return Data.table. Returns full data set with converted data variable, additional year variables and sorted by date.
 
 
 
-f.clean_dates <- function(x){
+f.clean_dates_courts_de <- function(x,
+                                    boundary = 50){
 
-    ## Variable "datum" als Datentyp "IDate" kennzeichnen
+    ## Convert "datum" to IDate class
     x$datum <- as.IDate(x$datum)
 
-    ## Variable "entscheidungsjahr" hinzufügen
+    ## Add variable "entscheidungsjahr"
     x$entscheidungsjahr <- year(x$datum)
 
-    ## Variable "eingangsjahr_iso" hinzufügen
-    x$eingangsjahr_iso <- f.year.iso(x$eingangsjahr_az)
+    ## Add variable "eingangsjahr_iso"
+    x$eingangsjahr_iso <- f.year.iso(x$eingangsjahr_az,
+                                     boundary = boundary)
 
-    ## Nach Datum sortieren
-    setorder(x,
-             datum)
+    ## Sort by Date
+    setorder(x, datum)
 
     return(x)
     
