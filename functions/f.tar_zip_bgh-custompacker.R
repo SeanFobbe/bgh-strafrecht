2092b6ee683a7c9c0a7b4dac5eb1c4d482ec6649
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2024-10-01 02:11:15 +0200

    comment out func

---
+++ functions/f.tar_zip_bgh-custompacker.R
@@ -0,0 +1,35 @@
+#' ZIP for targets framework: Custom packer for BGH Criminal Law Decisions
+#'
+#'
+#' @param x Character. The files to be added to the ZIP archive.
+#' @param names.new Character. The new file names of the files specified in "x".
+#' @param dir Character. The directory in which to create the ZIP archive.
+#'
+#' @return Character. The path to the ZIP file.
+
+## f.tar_zip_bgh_custompacker <- function(x,
+##                                        names.new,
+##                                        dir){
+
+
+    
+##     length(x)
+##     length(names.new)
+    
+    
+    
+##     filename <- file.path(dir, filename)
+    
+##     zip::zip(filename,
+##              x,
+##              mode = mode)
+    
+##     return(filename)
+    
+## }
+
+## DEBUGGING CODE
+
+## x <- tar_read(pdf.cleaned.noprob)
+## names.new <- tar_read(dt.final)$doc_id
+## dir <- "output"
